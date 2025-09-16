56db85e6d3ba3a5b86d0d445d481851122522ed2
kalema1 <kalemar8@gmail.com>
2025-09-16 08:11:29 +0300

    add reactive form controls

--- src/app/new-course-form/new-course-form.ts
+++ src/app/new-course-form/new-course-form.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-course-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './new-course-form.html',
-  styleUrl: './new-course-form.scss'
+  styleUrl: './new-course-form.scss',
 })
 export class NewCourseForm {
-
+  // create a new instance of FormGroup
+  signUpForm = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl(''),
+  });
 }
