584f0719e294edc75c5a4173fb483702511f141b
kalema1 <kalemar8@gmail.com>
2025-09-18 07:48:45 +0300

    add array of validators

--- src/app/new-course-form/new-course-form.ts
+++ src/app/new-course-form/new-course-form.ts
@@ -18,7 +18,10 @@ export class NewCourseForm {
   signUpForm = new FormGroup({
     // first value is the initial state of the form control the second value is the validators
     username: new FormControl('', Validators.required),
-    password: new FormControl('', Validators.required),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(5),
+    ]),
   });
 
   get username() {
