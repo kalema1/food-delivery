1f31afc2201cf400d1a9fc311823781de66a1add
kalema1 <kalemar8@gmail.com>
2025-09-18 07:37:48 +0300

    adding a validator

--- src/app/new-course-form/new-course-form.ts
+++ src/app/new-course-form/new-course-form.ts
@@ -1,16 +1,27 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormGroup,
+  FormControl,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-new-course-form',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './new-course-form.html',
   styleUrl: './new-course-form.scss',
 })
 export class NewCourseForm {
   // create a new instance of FormGroup
   signUpForm = new FormGroup({
-    username: new FormControl(''),
-    password: new FormControl(''),
+    // first value is the initial state of the form control the second value is the validators
+    username: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
   });
+
+  get username() {
+    return this.signUpForm.get('username');
+  }
 }
